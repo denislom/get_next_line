6e66c79193df40548de117a92ef658f098404f51
Denis Lom <lom.denis@seznam.cz>
2023-04-30 17:29:35 +0200

    get_next_line comment

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,14 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 17:11:44 by dlom              #+#    #+#             */
-/*   Updated: 2023/04/30 17:20:44 by dlom             ###   ########.fr       */
+/*   Updated: 2023/04/30 17:29:03 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+RETURN: the line that was read or NULL if an error occured or if there
+	is nothing else to be read.
+*/
 char	*get_next_line(int fd)
 {
 }
