99caa044e92658191d7bfdb4651e2d13786e6afa
Denis Lom <lom.denis@seznam.cz>
2023-05-07 13:22:55 +0200

    abrakadabra

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 17:11:44 by dlom              #+#    #+#             */
-/*   Updated: 2023/05/04 13:41:57 by dlom             ###   ########.fr       */
+/*   Updated: 2023/05/07 13:21:51 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@ RETURN: the line that was read or NULL if an error occured or if there
 	is nothing else to be read.
 Function that returns a line read from a file descriptor.
 */
+char	ft_i_read_file(int fd, char *char_absorber)
+{
+	
+}
+
 char	*get_next_line(int fd)
 {
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 17:07:11 by dlom              #+#    #+#             */
-/*   Updated: 2023/05/07 13:05:40 by dlom             ###   ########.fr       */
+/*   Updated: 2023/05/07 13:22:26 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1024
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 char	*get_next_line(int fd);
 
