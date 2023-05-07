fa8c6886baadd984365f4a018dbc6c6cd8240bf5
Denis Lom <lom.denis@seznam.cz>
2023-05-07 13:06:22 +0200

    BUFFER SIZE

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 17:11:44 by dlom              #+#    #+#             */
-/*   Updated: 2023/04/30 17:50:26 by dlom             ###   ########.fr       */
+/*   Updated: 2023/05/04 13:41:57 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 RETURN: the line that was read or NULL if an error occured or if there
 	is nothing else to be read.
+Function that returns a line read from a file descriptor.
 */
 char	*get_next_line(int fd)
 {
