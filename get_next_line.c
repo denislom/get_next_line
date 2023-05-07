d267f78355b06ddd360574c72d39f89f6127f4c5
Denis Lom <lom.denis@seznam.cz>
2023-05-07 13:44:04 +0200

    some little magic done

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,11 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 17:11:44 by dlom              #+#    #+#             */
-/*   Updated: 2023/05/07 13:21:51 by dlom             ###   ########.fr       */
+/*   Updated: 2023/05/07 13:41:09 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <get_next_line.h>
 /*
 RETURN: the line that was read or NULL if an error occured or if there
 	is nothing else to be read.
@@ -22,4 +23,15 @@ char	ft_i_read_file(int fd, char *char_absorber)
 
 char	*get_next_line(int fd)
 {
+	static char	*char_accumulator[2048];
+	char		*line;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	char_accumulator[fd] = ft_i_read_file(fd, char_accumulator[fd]);
+	if (char_accumulator[fd] == NULL)
+		return (NULL);
+	//line =
+	//char_accumulator[fd] =
+	//return(line)
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 17:07:11 by dlom              #+#    #+#             */
-/*   Updated: 2023/05/07 13:22:26 by dlom             ###   ########.fr       */
+/*   Updated: 2023/05/07 13:43:33 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
+
+#include <stddef.h>
 
 char	*get_next_line(int fd);
 
