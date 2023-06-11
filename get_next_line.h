f45c459e131d72be07e65b63568d2ad8d3871d8a
Denis Lom <lom.denis@seznam.cz>
2023-06-11 15:05:31 +0200

    norm correction

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 17:07:11 by dlom              #+#    #+#             */
-/*   Updated: 2023/05/07 13:43:33 by dlom             ###   ########.fr       */
+/*   Updated: 2023/06/11 15:05:15 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #  define BUFFER_SIZE 1024
 # endif
 
-#include <stddef.h>
+# include <stddef.h>
 
 char	*get_next_line(int fd);
 
