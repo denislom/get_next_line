f985b7a1a13bfacbd7d774fa39b54735f7ddfd18
Denis Lom <lom.denis@seznam.cz>
2023-06-15 13:22:49 +0200

    Maria is sitting on the table

--- test_gnl_main.c
+++ test_gnl_main.c
@@ -5,7 +5,7 @@
 
 #include <stdio.h>
 #include <fcntl.h>
-
+/*
 int main(void)
 {
 	char *line;
@@ -14,6 +14,43 @@ int main(void)
 	fd[0] = open("test0.txt", O_RDONLY);
 	line = get_next_line(fd[0]);
 	printf("fd: %d | %s\n", fd[0], line);
+	line = get_next_line(fd[1]);
+	printf("fd: %d | %s\n", fd[0], line);
 	free(line);
+	return (0);
+}
+*/
+int main(void)
+{
+	char *line;
+	int fd[2];
+	size_t	i;
+
+	fd[0] = open("test0.txt", O_RDONLY);
+	fd[1] = open("test1.txt", O_RDONLY);
+
+	i = 0;
+	while (i < 2)
+	{
+		line = get_next_line(fd[i]);
+		printf("fd: %d | %s\n", fd[i], line);
+		free(line);
+
+		line = get_next_line(fd[i]);
+		printf("fd: %d | %s\n", fd[i], line);
+		free(line);
+
+		line = get_next_line(fd[i]);
+		printf("fd: %d | %s\n", fd[i], line);
+		free(line);
+
+		line = get_next_line(fd[i]);
+		printf("fd: %d | %s\n", fd[i], line);
+		free(line);
+
+		close(fd[i]);
+		i++;
+	}
+
 	return (0);
 }
