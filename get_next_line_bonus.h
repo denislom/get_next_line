d9fa94c99c40d242004ed89734f543214b349215
Denis Lom <lom.denis@seznam.cz>
2023-06-18 12:58:45 +0200

    Bonus done

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,24 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 18:53:34 by dlom              #+#    #+#             */
-/*   Updated: 2023/06/17 18:53:35 by dlom             ###   ########.fr       */
+/*   Updated: 2023/06/18 12:52:35 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char	*str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
