b29e5742a93d59c200c4d3b60d964260c9286ad0
Denis Lom <lom.denis@seznam.cz>
2023-06-24 16:09:36 +0200

    valgrind added

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@
 
 cc -Wall -Wextra -Werror -g get_next_line.c get_next_line_utils.c test_gnl_main.c  -o test_gnl_main
 ./test_gnl_main
+
+valgrind --leak-check=full --show-leak-kinds=all ./test_gnl_main
